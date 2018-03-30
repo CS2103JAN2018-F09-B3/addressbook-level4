0aca2b9bb18e10d29254a48601192c12a0603ba5
Eldon-Chung <eldon.chung@u.nus.edu>
2018-03-30 23:04:17 +0800

    parser: Refactor CommandParsers to use TokenType when tokenizing args.

--- src/main/java/seedu/address/logic/parser/BuyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/BuyCommandParser.java
@@ -5,13 +5,11 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import java.util.EmptyStackException;
-import java.util.Stack;
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.BuyCommand;
-import seedu.address.logic.conditionalparser.Lexer;
-import seedu.address.logic.conditionalparser.Token;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -26,23 +24,30 @@ public class BuyCommandParser {
      */
     public BuyCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        Stack<Token> argStack = new Lexer().lex(args).getTokenStack();
-        //ArgumentMultimap argMultimap =
-        //        ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_TAG);
-
-        Index index;
-        double amountToAdd;
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenizeToArgumentMultimap(args, TokenType.PREFIXAMOUNT);
+        if (!arePrefixesPresent(argMultimap, TokenType.PREFIXAMOUNT)
+                || argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, BuyCommand.MESSAGE_USAGE));
+        }
 
         try {
-            index = ParserUtil.parseIndex(argStack.pop().getPattern());
-            amountToAdd = ParserUtil.parseDouble(argStack.pop().getPattern());
+            Index index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            double amountToAdd = ParserUtil.parseDouble(argMultimap.getValue(TokenType.PREFIXAMOUNT).get());
+            return new BuyCommand(index, amountToAdd);
         } catch (IllegalValueException ive) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, BuyCommand.MESSAGE_USAGE));
         } catch (EmptyStackException ese) {
             throw new ParseException(BuyCommand.MESSAGE_NOT_BOUGHT);
         }
 
-        return new BuyCommand(index, amountToAdd);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, TokenType... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 
 }
