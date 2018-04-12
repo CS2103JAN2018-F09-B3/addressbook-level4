bf3a79be53c5bbee90494248db72f60f94fc0a55
Eldon-Chung <eldon.chung@u.nus.edu>
2018-04-12 11:51:21 +0800

    CoinSubredditList: Add method to get RedditUrl associated with coin

--- src/main/java/seedu/address/commons/core/CoinSubredditList.java
+++ src/main/java/seedu/address/commons/core/CoinSubredditList.java
@@ -20,9 +20,19 @@ public class CoinSubredditList {
 
     private static final Map<String, String> COIN_CODE_TO_SUBREDDIT_MAP = new HashMap<>();
     private static final String COIN_CODE_TO_SUBREDDIT_FILEPATH = "data/CoinCodeToSubreddit.json";
+    private static final String REDDIT_URL = "https://www.reddit.com/r/";
 
     public static boolean isRecognized(Coin coin) {
         return COIN_CODE_TO_SUBREDDIT_MAP.containsKey(coin.getCode().toString());
+    }
+
+    /**
+     * Obtains the subreddit url associated with the {@code coin}.
+     * @param coin
+     * @return the subreddit url associated with the {@code coin}
+     */
+    public static String getRedditUrl(Coin coin) {
+        return REDDIT_URL + COIN_CODE_TO_SUBREDDIT_MAP.get(coin.getCode().toString());
     }
 
     /**
