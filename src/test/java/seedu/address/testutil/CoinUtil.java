2014740a33d1de3f8b8b8da820292515a84e11c3
Eldon-Chung <eldon.chung@u.nus.edu>
2018-04-13 01:55:39 +0800

    CoinUtil: Add getBuyCommand method based on index

--- src/test/java/seedu/address/testutil/CoinUtil.java
+++ src/test/java/seedu/address/testutil/CoinUtil.java
@@ -1,15 +1,26 @@
 package seedu.address.testutil;
 
+import static seedu.address.logic.parser.TokenType.PREFIX_AMOUNT;
 import static seedu.address.logic.parser.TokenType.PREFIX_CODE;
 import static seedu.address.logic.parser.TokenType.PREFIX_TAG;
+import static seedu.address.testutil.TestUtil.DECIMAL_STRING;
 
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.BuyCommand;
 import seedu.address.model.coin.Coin;
 
 /**
  * A utility class for Coin.
  */
 public class CoinUtil {
+
+    /**
+     * Returns a buy command string for buy the {@code coin}.
+     */
+    public static String getBuyCommand(Index index) {
+        return BuyCommand.COMMAND_WORD + " " + index.getOneBased() + "  " + PREFIX_AMOUNT + " " + DECIMAL_STRING;
+    }
 
     /**
      * Returns an add command string for adding the {@code coin}.
